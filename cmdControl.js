c1f155a62809345600ddd23cc65dc1ae03acdf1c
myexistence <nicholasamunford@gmail.com>
2025-01-29 17:35:18 -0500

    start adding js styles

---
+++ cmdControl.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+body.style.backgroundColor = "black";
+const firstSection = document.querySelector("#bio");
+firstSection.style.display = "none";
+const main = document.querySelector("main");
+main.style.display = "none"
+body.style.alignContent = "center";
